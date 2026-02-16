3043dae7f9b52eac343290493631d5cb54c9f13a
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-02-16 12:24:54 +0100

    feat(script): add script to deploy manually on render

---
+++ deploy-schema.js
@@ -0,0 +1,13 @@
+import { exec } from "child_process";
+
+exec("npx prisma db push --force-reset", (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Output:\n${stdout}`);
+});
