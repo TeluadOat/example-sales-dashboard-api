0af7a300d132a69e3afdf6be88c4bfb90c6b2942
Olubodun Adeola <olubodunadeola2000@gmail.com>
2026-03-15 07:44:16 +0100

    chore: setup Prisma singleton client with transaction pooler and disable prepared statements

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,21 @@
+// import { PrismaClient } from '@prisma/client';
+
+// export const prisma = new PrismaClient();
+
+
+// prismaClient.ts
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+    global.prisma ||
+    new PrismaClient({
+        log: ['query'],
+        // @ts-ignore
+        __internal: { usePreparedStatements: false }, // avoids pooler prepared statement errors
+    });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
